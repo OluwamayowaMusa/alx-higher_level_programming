9f27a3c1fd2911f15a58cc0e3c162413091ecac1
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-02 10:09:00 +0000

    If, else control

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+print(f"Last digit of {number} is", end = ' ')
+
+if (number >= 0):
+    lastDigit = number % 10
+else:
+    lastDigit = number % 10
+    if (lastDigit != 0):
+        lastDigit -= 10
+
+if (lastDigit > 5):
+    print(f"{lastDigit} and is greater than 5")
+elif (lastDigit == 0):
+    print(f"{lastDigit} and is 0")
+else:
+    print(f"{lastDigit} and is less than 6 and not 0")
