ca53805c2fd0d8b3633a9ec463cb1484073febb3
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-02 10:11:02 +0000

    If else control statements

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 
 number = random.randint(-10000, 10000)
-print(f"Last digit of {number} is", end = ' ')
+print(f"Last digit of {number} is", end=' ')
 
 if (number >= 0):
     lastDigit = number % 10
