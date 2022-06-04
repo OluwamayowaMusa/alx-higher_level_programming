028dfa324a325adbe60f3bc61716d014f6ce7426
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-04 03:08:56 +0000

    Interpreting byte code

---
+++ 0x02-python-import_modules/magic_calculation_102.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: First Integer
+        b: Second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """My subtraction function
+
+    Args:
+        a: First Integer
+        b: Second integer
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+
+
+def mul(a, b):
+    """My multiplication function
+
+    Args:
+        a: First Integer
+        b: Second integer
+
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """My Division function
+
+    Args:
+        a: First Integer
+        b: Second integer
+
+    Returns:
+        The return value. a / b
+    """
+    return int((a / b))
