9ca0e53a7739369143184892073b75d12c11d5fd
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-06 22:54:32 +0000

    Check if a list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,25 @@
 #include "lists.h"
 
+/**
+ * num_list - Check if linked list contain the same value
+ * @list: Pointer to linked list
+ *
+ * Return: 1 - same value
+ *         0 - otehrwise
+ */
+int num_list(listint_t *list)
+{
+	int value;
 
+	value = list->n;
+	while (list != NULL)
+	{
+		if (list->n != value)
+			return (0);
+		list = list->next;
+	}
+	return (1);
+}
 
 /**
  * add_nodeint - add node to linked list
@@ -32,7 +51,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *storeNode = NULL, *temp, *temp1, *temp2, *temp3;
+	listint_t *storeNode = NULL, *temp, *temp1, *temp2, *temp3, *temp4;
 	int ctrl = 1, i = 0, j = 0;
 
 	if (head == NULL)
@@ -41,9 +60,13 @@ int is_palindrome(listint_t **head)
 		return (1);
 	if ((*head)->next == NULL)
 		return (1);
-	temp = temp1 = temp3 = *head;
+	temp = temp1 = temp3 = temp4 = *head;
 	while (temp3 != NULL)
 		temp3 = temp3->next, i++;
+	if (num_list(temp4) == 1)
+		return (1);
+	if (i % 2 != 0)
+		return (0);
 	while (temp != NULL && temp->next != NULL)
 	{
 		if (temp->n == (temp->next)->n && j == (i / 2) - 1)
