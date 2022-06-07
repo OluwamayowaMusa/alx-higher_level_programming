53aaefac6a0a861ac3ddaf4a8975ca4795a86d22
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-07 08:37:07 +0000

    Check if linked list is palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,36 +1,15 @@
 #include "lists.h"
 
 /**
- * num_list - Check if linked list contain the same value
- * @list: Pointer to linked list
- *
- * Return: 1 - same value
- *         0 - otehrwise
- */
-int num_list(listint_t *list)
-{
-	int value;
-
-	value = list->n;
-	while (list != NULL)
-	{
-		if (list->n != value)
-			return (0);
-		list = list->next;
-	}
-	return (1);
-}
-
-/**
- * add_nodeint - add node to linked list
- * @head: pointer to pointer to linked list
+ * add_nodeint - add node to the end of linked list
+ * @head: Pointer to pointer to linked list
  * @n: Data
  *
- * Return: Address of new Node
+ * Return: Address of linked list
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t  *newNode = NULL;
+	listint_t *newNode = NULL;
 
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
@@ -41,53 +20,40 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	return (newNode);
 }
 
-
 /**
- * is_palindrome - Checks if a linked list is a panlindrome
- * @head: Pointer to pointer to linked List
+ * is_panlindrome - check if a linked list is palindrome
+ * @head: Pointer to pointer to linked list
  *
- * Return: 0 - if not a palindrome
- *         1 - if it is a palindrome
+ * Return: 1 - if palindrome
+ *         0 - if not palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *storeNode = NULL, *temp, *temp1, *temp2, *temp3, *temp4;
-	int ctrl = 1, i = 0, j = 0;
+	listint_t *storeNode = NULL, *temp = NULL, *temp1 = NULL;
+	int res = 1;
 
 	if (head == NULL)
 		return (0);
-	if (*head == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	if ((*head)->next == NULL)
-		return (1);
-	temp = temp1 = temp3 = temp4 = *head;
-	while (temp3 != NULL)
-		temp3 = temp3->next, i++;
-	if (num_list(temp4) == 1)
-		return (1);
-	if (i % 2 != 0)
-		return (0);
-	while (temp != NULL && temp->next != NULL)
-	{
-		if (temp->n == (temp->next)->n && j == (i / 2) - 1)
-		{
-			temp = temp->next;
-			break;
-		}
-		temp = temp->next, j++;
-	}
+	temp = *head;
 	while (temp != NULL)
-		add_nodeint(&storeNode, temp->n), temp = temp->next;
-	temp2 = storeNode;
-	while (storeNode != NULL)
 	{
-		if (temp1->n != storeNode->n)
+		add_nodeint(&storeNode, temp->n);
+		temp = temp->next;
+	}
+	temp1 = storeNode;
+	temp = *head;
+	while (storeNode != NULL && temp != NULL)
+	{
+		if (storeNode->n != temp->n)
 		{
-			ctrl = 0;
+			res = 0;
 			break;
 		}
-		storeNode = storeNode->next, temp1 = temp1->next;
+		storeNode = storeNode->next;
+		temp = temp->next;
 	}
-	free_listint(temp2);
-	return (ctrl);
+	free_listint(temp1);
+	return (res);
 }
--- 0x03-python-data_structures/test13.c
+++ 0x03-python-data_structures/test13.c
@@ -10,8 +10,8 @@ int main(void)
 	listint_t *head;
 
 	head = NULL;
-	add_nodeint_end(&head, 1);
-	add_nodeint_end(&head, 2);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 100);
 	print_listint(head);
 
 	if (is_palindrome(&head) == 1)
