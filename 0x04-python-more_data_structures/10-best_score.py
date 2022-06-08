54937f15ad3d1539950e5dedee106abaab2f8fca
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-08 22:33:49 +0000

    Best Score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -9,17 +9,6 @@ def best_score(a_dictionary):
         return (None)
     keyList = list(a_dictionary.keys())
     valList = list(a_dictionary.values())
-    maxval = None
-    res = None
-    if (len(valList) == 0):
-        return (None)
-    for index, i in enumerate(valList):
-        if (index == 0):
-            maxVal = i
-        else:
-            if (i > maxVal):
-                maxVal = i
-                res = index
-    if (valList.count(maxVal) != 1):
-        return (None)
-    return (keyList[res])
+    maxVal = max(valList)
+    index = valList.index(maxVal)
+    return (keyList[index])
