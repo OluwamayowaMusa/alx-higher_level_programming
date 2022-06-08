499326b16dfc064dc03ce75a6e6e158b5379b7cc
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-08 15:24:29 +0000

    Key with Biggest integr Value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    """Returns the key with the biggest value"""
+    if (a_dictionary is None):
+        return (None)
+    keyList = list(a_dictionary.keys())
+    valList = list(a_dictionary.values())
+    maxval = None
+    res = None
+    if (len(valList) == 0):
+        return (None)
+    for index, i in enumerate(valList):
+        if (index == 0):
+            maxVal = i
+        else:
+            if (i > maxVal):
+                maxVal = i
+                res = index
+    return (keyList[res])
