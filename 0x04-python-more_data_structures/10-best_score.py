c3c4fe292a697c0bb662f2904a59434742c0c5a3
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-08 21:25:22 +0000

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,6 +5,8 @@ def best_score(a_dictionary):
     """Returns the key with the biggest value"""
     if (a_dictionary is None):
         return (None)
+    if (len(a_dictionary) == 0):
+        return (None)
     keyList = list(a_dictionary.keys())
     valList = list(a_dictionary.values())
     maxval = None
