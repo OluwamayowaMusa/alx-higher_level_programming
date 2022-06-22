4a5db7afaa62c97b3d6e5d0791e26d99e149ed43
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-22 13:47:01 +0000

    Understanding bytecode relating to class

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import math
+
+""" A module which defines the MagicClass.
+
+"""
+
+
+class MagicClass:
+    """ Describes a circle.
+
+    """
+
+    def __init__(self, radius=0):
+        """ Initializes the object with radius.
+
+        Args:
+            radius (int, float): Radius of circle
+        """
+        if type(radius) is not int or type(radius) is not float:
+            raise TypeError("radius must be a number")
+        else:
+            self.__radius = radius
+
+    def area(self):
+        """ Calculates the area of circle """
+        return (self.__radius ** 2) * math.pi
+
+    def cicumference(self):
+        """ Calculates the circumference of circe """
+        return 2 * math.pi * self.__radius
