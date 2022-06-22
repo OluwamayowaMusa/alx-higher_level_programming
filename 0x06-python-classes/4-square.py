9e602008e3ea44e60b868ef5063ef602a412f751
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-22 04:35:29 +0000

    A square class with attribute size using property

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+""" A module which defines a Square class.
+
+"""
+
+
+class Square:
+    """ Describes a Square.
+
+    """
+
+    def __init__(self, size):
+        """ Initializes the object with size.
+
+        Args:
+            size (int): Size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """ Gets the size of the sqaure object. Also check the type
+        of argument and return the right error
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Calculates the area of the square"""
+        return self.size ** 2
