8145688579b72fce19a1c1232b1ca769d0086e8f
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-22 14:13:12 +0000

    Understanding bytecode relating to class

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -17,7 +17,7 @@ class MagicClass:
             radius (int, float): Radius of circle
         """
         self.__radius = radius
-        if type(radius) is not int or type(radius) is not float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
