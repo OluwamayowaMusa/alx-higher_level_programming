92e1387f2351f6fb87d401181e4339db14852ec1
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-25 11:29:52 +0000

    Writing a doctest for function text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+""" Defines a function that print new lines after '.', '?' and ':'.
+
+"""
+
+
+def text_indentation(text):
+    """ Prints new line after ., ? and :.
+
+    Args:
+        text (str): String passed
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    _length = 0
+    while _length < len(text):
+        _c = text[_length]
+        if _c in ".?:":
+            print(_c, end='')
+            print()
+            print()
+            _length += 1
+        else:
+            print(_c, end='')
+        _length += 1
