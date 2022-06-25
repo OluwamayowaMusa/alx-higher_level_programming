8a02e987754c46b53ea2102bac0b5e213b1a2096
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-25 12:24:37 +0000

    Writing a unittest case for defined function

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Unittest for max_integer([...])
+"""
+
+import unittest
+max_integer = __import__("6-max_integer").max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """ Methods max_integer function.
+
+    """
+
+    def test_max_integer(self):
+        """ TestCase for right output """
+        self.assertEqual(max([1, 2, 8, 6, 3]), max_integer([1, 2, 8, 6, 3]))
+        self.assertEqual(max([9, 99, 100]), max_integer([99, 99, 100]))
+        self.assertEqual(None, max_integer([]))
+        self.assertEqual(max([-97, -5, -6, -1]),
+                         max_integer([-97, -5, -6, -1]))
