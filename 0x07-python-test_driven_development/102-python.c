ca203967d23904b40d4d8a9cd9a678393ecaa0c4
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-26 14:05:52 +0000

    Cpython

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -10,6 +10,7 @@ void print_python_string(PyObject *p)
 {
 	PyTypeObject *temp;
 	char *data;
+	Py_ssize_t len;
 
 	puts("[.] string object info");
 	if (!PyUnicode_Check(p))
@@ -17,10 +18,11 @@ void print_python_string(PyObject *p)
 		puts("  [ERROR] Invalid String Object");
 		return;
 	}
+	len = PyUnicode_GET_SIZE(p);
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 	{
 		puts("  type: compact ascii");
-		printf("  length: %lu\n", ((PyASCIIObject *)p)->length);
+		printf("  length: %lu\n", len);
 		p = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
 		data = PyBytes_AS_STRING(p);
 		printf("  value: %s\n", data);
@@ -28,7 +30,7 @@ void print_python_string(PyObject *p)
 	else if (PyUnicode_IS_COMPACT(p) && !PyUnicode_IS_ASCII(p))
 	{
 		puts("  type: compact unicode object");
-		printf("  length: %lu\n", ((PyCompactUnicodeObject *)p)->wstr_length);
+		printf("  length: %lu\n", len);
 		p = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
 		data = PyBytes_AS_STRING(p);
 		printf("  value: %s\n", data);
