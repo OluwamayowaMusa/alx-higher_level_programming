1fafff6408d5bafd2cba0a90bfa28e08fd7290cf
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-07-06 19:10:07 +0000

    Write to file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Defines a function that writes to file.
+
+"""
+
+
+def write_file(filename="", text=""):
+    """ Writes text to filename.
+
+    Args:
+        filename (str): File passed
+        text (str): Text to write to file
+    """
+
+    if len(filename) != 0:
+        with open(filename, 'w', encoding='utf-8') as f:
+            no_chars = f.write(text)
+        return no_chars
+    return 0
