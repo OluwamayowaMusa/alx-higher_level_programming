56721575a37486f74c6b0ca7fbfca1eacb047c8f
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-07-06 08:20:26 +0000

    Add attribute to an  object

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -12,6 +12,9 @@ def add_attribute(object_, attr, value):
         attr: attribute
         value: value
     """
-    if isinstance(object_, (str, int, list, float, dict, tuple)):
+    if type(object_) in (str, int, list, float, dict, tuple):
         raise TypeError("can't add new attribute")
+    if hasattr(object_, "__slots__"):
+        if attr not in object_.__slots__:
+            raise TypeError("can't add new attribute")
     setattr(object_, attr, value)
--- 0x0A-python-inheritance/101-main.py
+++ 0x0A-python-inheritance/101-main.py
@@ -6,12 +6,23 @@ class MyClass():
     pass
 
 
+class LockedClass:
+    __slots__ = ["first_name"]
+
+
 mc = MyClass()
 add_attribute(mc, "name", "John")
 print(mc.name)
 
 try:
     a = "My String"
+    add_attribute(a, "name", "Bob")
+    print(a.name)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    a = LockedClass()
     add_attribute(a, "name", "Bob")
     print(a.name)
 except Exception as e:
