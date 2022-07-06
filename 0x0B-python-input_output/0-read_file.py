87a23281bf16a9e797dff1ad2cde2e127a26f1a7
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-07-06 18:58:46 +0000

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Defines a function that reads a text file.
+
+"""
+
+
+def read_file(filename=''):
+    """ Reads a text file and prints to stdout.
+
+    Args:
+        filename (str): File name / path
+    """
+
+    if len(filename) != 0:
+        with open(filename, 'r', encoding='utf-8') as f:
+            read_data = f.read()
+        print(read_data.strip())
