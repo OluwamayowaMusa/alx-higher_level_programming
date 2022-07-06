1b2062e8578e72177ad4644a0cbae937c3d4bf11
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-07-06 06:10:38 +0000

    Initialized __str__

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Defines a class which inherits from Class Rectangle.
+
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ Square class inherits from Rectangle.
+
+    """
+
+    def __init__(self, size):
+        """ Initializes Object.
+
+        Args:
+            size (int): Size of object
+
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """ String Representation of object """
+        return "[Square] {0}/{0}".format(self.__size)
+
+    def area(self):
+        """ Calculates Area square """
+        return self.__size ** 2
