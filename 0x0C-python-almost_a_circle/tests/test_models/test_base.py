7a95a0fc977eb4a1d3cf010608f091a9e96705dc
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-07-17 16:23:33 +0000

    Cleared all test cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -11,37 +11,4 @@ class TestBaseClass(unittest.TestCase):
     """ Test cases for the Base class.
 
     """
-
-    def setUp(self):
-        """ Setup the test case """
-        self.b1 = Base()
-        self.b2 = Base(12)
-        self.b3 = Base()
-
-    def test_id(self):
-        """ Test id values of instances of Base class """
-        self.assertEqual(self.b1.id, 3)
-        self.assertEqual(self.b2.id, 12)
-        self.assertEqual(self.b3.id, 4)
-
-    def test_to_json_string(self):
-        """ Test method to_json_string """
-        self.assertEqual(Base.to_json_string(None), '[]')
-        self.assertEqual('[]', Base.to_json_string([]))
-        self.assertEqual(Base.to_json_string([{"id": 12}]), '[{"id": 12}]')
-
-    def test_to_json_string_empty_list(self):
-        """ Test method to_json_string for empty list """
-        self.assertEqual(Base.to_json_string([]), "[]");
-
-    def test_from_json_string(self):
-        """ Test method from_json_string """
-        self.assertListEqual(Base.from_json_string(None), [])
-        self.assertListEqual(Base.from_json_string("[]"), [])
-        self.assertListEqual(Base.from_json_string('[{"id": 12}]'), [{"id": 12}])
-
-    def tearDown(self):
-        """ Dispose Created Object """
-        del self.b1
-        del self.b2
-        del self.b3
+    pass
