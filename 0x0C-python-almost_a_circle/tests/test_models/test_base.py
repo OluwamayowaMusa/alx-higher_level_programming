a581f378d8ea893fc8e1eba957863cce5637c221
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-07-17 16:48:35 +0000

    Test id for base class

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -11,21 +11,12 @@ class TestBaseClass(unittest.TestCase):
     """ Test cases for the Base class.
 
     """
-    @classmethod
-    def setUpClass(cls):
-        """ Test Examples for test methods """
-        cls.b1 = Base()
-        cls.b2 = Base()
-        cls.b3 = Base(89)
-
     def test_id(self):
-        self.assertEqual(self.b1.id, 1)
-        self.assertEqual(self.b2.id, 2)
-        self.assertEqual(self.b3.id, 89)
+        """ Test id of base Object """
+        b1 = Base()
+        b2 = Base()
+        b3 = Base(89)
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 89)
 
-    @classmethod
-    def tearDownClass(cls):
-        """ Delete test examples """
-        del cls.b1
-        del cls.b2
-        del cls.b3
