35503e03206e1f354588c4c8fb8672cc379cc07f
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-07-18 10:38:42 +0000

    Implemented Unittest for to_dictionary, update methods

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -87,6 +87,40 @@ class TestRectangleClass(unittest.TestCase):
         self.assertEqual(len(lines), self.r3.height + self.r3.y)
         self.assertEqual(len(lines[-1]) - 1, self.r3.width + self.r3.x)
 
+    def test_to_dictionary(self):
+        """ Test method to_dictionary of Rectangle Object """
+        self.assertDictEqual(self.r1.to_dictionary(),
+                             {'id': 3, 'x': 0, 'y': 0, 'width': 1,
+                             'height': 2})
+        self.assertDictEqual(self.r2.to_dictionary(),
+                             {'id': 4, 'x': 3, 'y': 0, 'width': 1,
+                             'height': 8})
+
+    def test_update(self):
+        """ Test method update of Rectangle Object """
+        self.r1.update()
+        self.assertEqual(self.r1.id, 3)
+        self.r1.update(50)
+        self.assertEqual(self.r1.id, 50)
+        self.r1.update(50, 5)
+        self.assertEqual(self.r1.width, 5)
+        self.r1.update(50, 5, 3)
+        self.assertEqual(self.r1.height, 3)
+        self.r1.update(50, 5, 3, 8)
+        self.assertEqual(self.r1.x, 8)
+        self.r1.update(50, 5, 3, 8, 9)
+        self.assertEqual(self.r1.y, 9)
+        self.r2.update(**{'id': 75})
+        self.assertEqual(self.r2.id, 75)
+        self.r2.update(**{'id': 75, 'width': 3})
+        self.assertEqual(self.r2.width, 3)
+        self.r2.update(**{'id': 75, 'width': 3, 'height': 6})
+        self.assertEqual(self.r2.height, 6)
+        self.r2.update(**{'id': 75, 'width': 3, 'height': 6, 'x': 5})
+        self.assertEqual(self.r2.x, 5)
+        self.r2.update(**{'id': 75, 'width': 3, 'height': 6, 'x': 5, 'y': 7})
+        self.assertEqual(self.r2.y, 7)
+
     @classmethod
     def tearDownClass(cls):
         """ Deletes test Examples """
