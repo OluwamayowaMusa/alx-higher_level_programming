9f79c0341e7a4f1df096c98a5afe6fc4bd6e1722
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-08-11 07:24:41 +0000

    Convert a database to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,4 @@
 -- Converts hbtn_0c_0 database to UTF8
 ALTER database hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER table first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER table hbtn_0c_0.first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
