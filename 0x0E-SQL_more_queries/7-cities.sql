f76f54a43b7caf10f40f3e7cd7e9c59f51bab858
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-08-13 22:29:57 +0000

    Create a table with FOREIGN KEY constraint

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,7 @@ CREATE database IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE table IF NOT EXISTS cities
   (id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT,
-   state_id INT NOT NULL, FOREIGN KEY(id) REFERENCES states(id),
+   state_id INT NOT NULL, 
+   FOREIGN KEY(state_id) REFERENCES states(id),
    name VARCHAR(256) NOT NULL
   );
