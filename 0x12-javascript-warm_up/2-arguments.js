8b62475bf7aad0bf234fb0bfb62d1ccaa0da6858
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-09-06 04:32:07 +0000

    Working with conditionals in javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 import { argv } from 'node:process';
 
-if (argv[2]) {
+if (argv[2] && argv[3]) {
+  console.log('Arguments found');
+} else if (argv[2]) {
   console.log('Argument found');
-} else {
+}
+else {
   console.log('No argument');
 }
