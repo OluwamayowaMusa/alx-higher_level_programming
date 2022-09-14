c3e8f212e8037a6f2685b75af5c9b6c7d61d036e
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-09-14 02:05:45 +0000

    Fixed Error in task two and completed task 3(safe injection)

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-""" Filtering result from database based on User Input
+""" Using queries safe from SQL injection
 
 """
 import sys
 import MySQLdb
 
 
-def filter_user():
-    """ Filter result using user input condition """
+def safe_query():
+    """ Access a database with a query safe from SQL
+        injection
+    """
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3],
                          port=3306)
     cur = db.cursor()
+    user_input = sys.argv[4].split()
     cur.execute('SELECT * FROM states WHERE name = \'{}\''
-                'ORDER BY id ASC'.format(sys.argv[4]))
+                'ORDER BY id ASC'.format(user_input[0]))
     for row in cur.fetchall():
         print(row)
 
 
 if __name__ == "__main__":
-    filter_user()
+    safe_query()
