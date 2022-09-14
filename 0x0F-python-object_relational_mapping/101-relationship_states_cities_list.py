2136d6ad94b51d34b7dce3a7afd4cc4922911d0e
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-09-14 22:37:14 +0000

    Test

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -16,7 +16,7 @@ def relationship_property():
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).join(City).order_by(State.id).order_by(City.id):
+    for state in session.query(State).order_by(State.id):
         print(f"{state.id}: {state.name}")
         for city in state.cities:
             print(f"    {city.id}: {city.name}")
