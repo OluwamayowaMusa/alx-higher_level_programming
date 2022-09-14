9fd2348384b8a06d3c6bb26dbe30900756105751
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-09-14 12:34:07 +0000

    Used SQLAlchemy to fetch data

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,11 +7,17 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql+mysqldb://{}:{}'
+
+def fetch_data():
+    """ Fetch data from database using SQLAlchemy """
+    engine = create_engine('mysql+mysqldb://{}:{}'
                        '@localhost/{}'.format(sys.argv[1],
                                               sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id):
+        print(f"{state.id}: {state.name}")
 
-Session = sessionmaker(bind=engine)
-session = Session()
-for state in session.query(State).order_by(State.id):
-    print(f"{state.id}: {state.name}")
+
+if __name__ == "__main__":
+    fetch_data()
