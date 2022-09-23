0a7aa75e16e78c6b38903b3488f348c7cd5c892b
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-09-23 22:33:38 +0000

    Using the JSON format

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Using JSON format in requests package
+
+"""
+import sys
+import requests
+
+
+def json_api(url=None, data=None):
+    """ Using the JSON format
+
+    """
+
+    if url:
+        response = requests.post(url, data={'q': data})
+        user_dict = response.json()
+        if user_dict:
+            print(f"[{user_dict['id']}] {user_dict['name']}")
+        else:
+            print("No result")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        json_api(url="http://0.0.0.0:5000/search_user", data="")
+    else:
+        json_api(url="http://0.0.0.0:5000/search_user", data=sys.argv[1])
