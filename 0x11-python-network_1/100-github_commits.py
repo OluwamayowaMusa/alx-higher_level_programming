220143dee75b63677788219c64c7d8424c3523bb
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-09-24 11:52:36 +0000

    Parsing the information gotten from github api

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Getting the most recent commits in a respository using GITHUB api
+
+"""
+import sys
+import requests
+
+
+def github_commit(url=None, user=None, passwd=None):
+    """ Using the Github api
+
+    Args:
+        url (str): URL passsed
+        user (str): UserName of User
+        passwd (str): Password of user
+    """
+
+    if url and user and passwd:
+        response = requests.get(url, auth=(user, passwd))
+        data = response.json()
+        result = []
+        for user in data:
+            result.append((user["sha"], user["commit"]["author"]["name"],
+                           user["commit"]["author"]["date"]))
+        data = sorted(result, key=lambda item: item[2], reverse=True)
+        for user in data[:10]:
+            print(f"{user[0]}: {user[1]}")
+
+
+if __name__ == "__main__":
+    github_commit(url="https://api.github.com/repos/"
+                  "{}/{}/commits".format(sys.argv[1], sys.argv[2]),
+                  user="OluwamayowaMusa",
+                  passwd="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")
