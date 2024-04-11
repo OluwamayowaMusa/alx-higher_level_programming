fc0dff4b2d4fc7b6c7e351121460774ee8401142
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2024-04-11 17:30:31 +0100

    Changed length to size

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const commandLineArg = process.argv;
-if (commandLineArg.length === 2) {
+if (commandLineArg.size === 2) {
   console.log('No Argument');
 } else {
   console.log(commandLineArg[2]);
