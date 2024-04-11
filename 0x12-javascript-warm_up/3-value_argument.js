74ac61fb4b78022b8cd69262e9c12a69fb56860b
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2024-04-11 17:26:26 +0100

    Updated  task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-
 const commandLineArg = process.argv;
 if (commandLineArg.length === 2) {
   console.log('No Argument');
 } else {
-  console.log(commandLineArg[2])
+  console.log(commandLineArg[2]);
 }
