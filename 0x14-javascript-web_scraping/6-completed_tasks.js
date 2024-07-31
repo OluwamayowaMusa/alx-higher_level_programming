231123a6edc600e34f036d28eadf8911a886f9c1
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2024-07-31 06:34:51 +0100

    Final

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -11,13 +11,13 @@ request(url, (error, _response, body) => {
   const totalTodos = JSON.parse(body);
   const completedTodo = {};
   for (const todo of totalTodos) {
-    if (!(todo.userId in completedTodo)) {
-        completedTodo[todo.userId] = 0;
+    if (todo.userId in completedTodo) {
+      if (!(todo.userId in completedTodo)) {
+        completedTodo[todo.userId] = 1;
+      } else {
+        completedTodo[todo.userId] += 1;
+      }
     }
-    if (todo.completed === true) {
-      completedTodo[todo.userId] += 1;
-    }
-
   }
   console.log(completedTodo);
 });
