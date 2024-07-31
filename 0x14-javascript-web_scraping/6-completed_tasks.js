a0b2fea68ad89b14b11f3b0dd8d3368f52fbfaeb
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2024-07-31 06:01:21 +0100

    Test again

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,22 @@
 #!/usr/bin/node
-
 const request = require('request');
+
 const url = process.argv[2];
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const completed = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      const task = tasks[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
-        }
-      }
-    }
-    console.log(completed);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
+request(url, (error, _response, body) => {
+  if (error) {
+    console.log(error);
+    return;
   }
+  const totalTodos = JSON.parse(body);
+  const completedTodo = {};
+  for (const todo of totalTodos) {
+    if (!(todo.userId in completedTodo)) {
+      completedTodo[todo.userId] = 0;
+    }
+    if (todo.completed === true) {
+      completedTodo[todo.userId] += 1;
+    }
+  }
+  console.log(completedTodo);
 });
